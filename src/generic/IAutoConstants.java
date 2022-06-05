7af4cbba37dc74126e8f8640932cb7500b11bf83
Aishwarya D C <aishwaryadc27@gmail.com>
2022-06-05 13:18:56 +0530

    Update

--- src/generic/IAutoConstants.java
+++ src/generic/IAutoConstants.java
@@ -3,6 +3,7 @@ package generic;
 public interface IAutoConstants 
 {
 	//modification 
+	//mmmm
 		String CHROME_KEY="webdriver.chrome.driver";
 		String CHROME_VALUE="./Drivers/chromedriver.exe";
 		String GECKO_KEY="webdriver.gecko.driver";
